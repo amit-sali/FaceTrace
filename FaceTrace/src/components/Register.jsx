e4188a99edb544853e3c2f66ff49f43c36737070
amit-sali <amitsaliap@gmail.com>
2025-01-16 17:01:57 +0530

    hero page crated

---
+++ FaceTrace/src/components/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <>
+     <h1>Log in</h1>
+     <form>
+        <input type='text'  placeholder='email' />
+
+        <input type='password'  placeholder='password' />
+     </form>
+    
+    </>
+   
+  )
+}
+
+export default Register ;
