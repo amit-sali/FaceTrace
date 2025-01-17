069fcc51db4175b866529e51101623146b449cd7
amit-sali <amitsaliap@gmail.com>
2025-01-17 18:59:50 +0530

    Refining navbar functionality 2

--- FaceTrace/src/components/Navbar.jsx
+++ FaceTrace/src/components/Navbar.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
 
-
-
 function Navbar() {
   return (
     <>
@@ -13,7 +11,7 @@ function Navbar() {
           FaceTrace
         </div>
 
-        {/* Navigation Links */}
+        {/* Navigation Links , only change color of active link */}
         <div className="space-x-6">
           <NavLink to="/" className={({ isActive }) => isActive ? "text-gray-700 font-bold" : "text-black hover:text-gray-700 transition duration-300"}>Home</NavLink>
           <NavLink to="/about" className={({ isActive }) => isActive ? "text-gray-700 font-bold" : "text-black hover:text-gray-700 transition duration-300"}>About</NavLink>
@@ -21,12 +19,6 @@ function Navbar() {
         </div>
       </div>
     </nav>
-    
-    
-    
-    
-    
-    
     </>
   )
 }
