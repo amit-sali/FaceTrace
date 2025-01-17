8179e90293e09dfb217c3060d8cf3ba1adadae05
amit-sali <amitsaliap@gmail.com>
2025-01-17 18:36:30 +0530

    simple navbar is added as a foundation start

--- FaceTrace/src/App.jsx
+++ FaceTrace/src/App.jsx
@@ -4,6 +4,7 @@ import SignUp from './components/Signup'
 import Login from './components/Login'
 import {Routes, Route} from 'react-router-dom'
 import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 // { Switch is now Route in react-router-dom v6} 
 function App() {
@@ -11,9 +12,9 @@ function App() {
   return (
     <>
     {/* <Card/> */}
-
+      <Navbar/>
       <Routes>
-        <Route exact path='/' Component={Home} />
+        {/* <Route exact path='/' Component={Home} /> */}
         <Route exact path='/login' Component={Login} />
         <Route exact path='/register' Component={SignUp} />
       </Routes>
--- FaceTrace/src/components/Navbar.jsx
+++ FaceTrace/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-white text-black px-6 py-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <div className="text-2xl font-semibold">
+          FaceTrace
+        </div>
 
-export default Navbar
+        {/* Navigation Links */}
+        <div className="space-x-6">
+          <button className="text-black hover:text-gray-700 transition duration-300">Home</button>
+          <button className="text-black hover:text-gray-700 transition duration-300">About</button>
+          <button className="text-black hover:text-gray-700 transition duration-300">Login</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
